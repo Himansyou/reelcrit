b72d0270759cb64d474c9ffb393329532fa83678
Himanshu Rawat <himanshurawat2k4@gmail.com>
2025-03-13 20:28:40 +0530

    test commit

---
+++ src/main/java/com/himanshu/reelcrit/ReelcritApplication.java
@@ -0,0 +1,13 @@
+package com.himanshu.reelcrit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelcritApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelcritApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/himanshu/reelcrit/Users.java
@@ -0,0 +1,11 @@
+package com.himanshu.reelcrit;
+import lombok.Data;
+
+@Data
+public class Users {
+
+    private String username;
+    private String password;
+
+    
+}
